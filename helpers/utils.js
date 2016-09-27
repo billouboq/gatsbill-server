d65ff64144dead680aa734aeab6cb45c040573b6
billouboq <aurelien.boquet.web@gmail.com>
2016-09-27 22:38:14 +0200

    doc

--- helpers/utils.js
+++ helpers/utils.js
@@ -2,6 +2,12 @@
 
 module.exports = {keysIn};
 
+/**
+ * Check if all keys (provided in an array) are in an object
+ * @param  {Object} object The object you want to verify
+ * @param  {Keys} keys     An array of key
+ * @return {Boolean}       Return true if ALL keys are in the object
+ */
 function keysIn(object, keys) {
 
    if (!object || !keys) {

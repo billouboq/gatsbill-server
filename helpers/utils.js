2e7e0dbf8f6241c453d6c411816eb97fb4336c28
billouboq <aurelien.boquet.web@gmail.com>
2016-10-09 19:04:01 +0200

    remove throw

--- helpers/utils.js
+++ helpers/utils.js
@@ -13,14 +13,10 @@ module.exports = {keysIn, signPassword, verifyPassword};
  */
 function keysIn(object, keys) {
 
-   if (!object || !keys) {
-      throw new Error('Missing parameters');
-   }
-
    if (Array.isArray(keys) && keys.length) {
       return keys.filter(key => object.hasOwnProperty(key)).length === keys.length;
    }
-
+   
 }
 
 /**
